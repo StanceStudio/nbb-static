81451fea15c5c6fe26bcf68ffbb0a60f5c883e96
Jack Gregory <jack@stance.agency>
2020-04-23 11:22:07 +0100

    Make sure we have equal columns

--- components/ImageList.vue
+++ components/ImageList.vue
@@ -2,19 +2,19 @@
 <div class="md:flex">
   <div
       v-if="section.il_image.url"
-      class="lazy md:w-5/12 mb-10 md:mb-0">
+      class="lazy md:w-5/12 mb-10 md:mb-0 md:pr-8 lg:pr-14 xxl:pr-20">
       <div class="lazy">
         <img v-lazy="section.il_image.url" :alt="section.il_image.alt" />
       </div>
     </div>
     <div
       v-if="section.il_text_items"
-      class="md:w-1/2 md:pl-8 lg:pl-14 xxl:pl-20 flex flex-col justify-between">
+      class="flex flex-col">
       <div
         v-for="(text, i) in section.il_text_items"
         :key="'section-' + i"
-        class="uppercase font-serif text-xl lg:text-2xl flex md:flex-nowrap flex-wrap">
-          {{ text.text_1 }} <DashLine class="inline-block px-4" style="width:3em"/> {{ text.text_2 }}
+        class="uppercase font-serif text-xl lg:text-2xl flex md:flex-no-wrap flex-wrap items-center">
+          <div ref="text1" :style="{width: itemWidth}">{{ text.text_1 }}</div> <DashLine class="inline-block px-4" style="width:3em;height:1rem"/> <div>{{ text.text_2 }}</div>
       </div>
     </div>
   </div>
@@ -22,6 +22,18 @@
 
 <script>
 import DashLine from '~/assets/svg/DashLine.svg?inline';
+
+const debounce = function(fn, delay) {
+  var timeoutID = null
+  return function () {
+    clearTimeout(timeoutID)
+    var args = arguments
+    var that = this
+    timeoutID = setTimeout(function () {
+      fn.apply(that, args)
+    }, delay)
+  }
+}
 
 export default {
   name: "ImageList",
@@ -34,8 +46,39 @@ export default {
     DashLine
   },
 
-  computed: {
+  data() {
+    return {
+      itemWidth: '',
+    }
+  },
 
+  mounted() {
+    const _that = this;
+
+    this.calculateItemWidths();
+
+    if (process.browser) {
+      window.addEventListener('resize', function() {
+        debounce(_that.calculateItemWidths(), 500);
+      });
+    }
+  },
+
+  methods: {
+    calculateItemWidths() {
+      const widths = [];
+      this.itemWidth = '';
+
+      this.$nextTick(() => {
+        this.$refs.text1.forEach((item, i) => {
+            const w = item.getBoundingClientRect().width;
+            //console.log('hw',w);
+            widths.push(w);
+        });
+
+        this.itemWidth = Math.max(...widths) + 'px';
+      });
+    }
   }
 }
 </script>
